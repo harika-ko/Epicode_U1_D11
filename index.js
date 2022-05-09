2029830bd3dde6d1dfc52224e654e1407a6f5d3d
Harika_Kommuri <harikakommuri35@gmail.com>
2022-05-09 14:58:44 +0200

    Solved Exercise 7

---
+++ index.js
@@ -0,0 +1,7 @@
+//EX7: Write a function to change the h1 text to something else
+
+function changeH1() {
+    let h1Node = document.getElementById("heading")
+    h1Node.innerText = "Hello! I am the new Heading"
+}
+
